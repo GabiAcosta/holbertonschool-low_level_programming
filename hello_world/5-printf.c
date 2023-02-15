67cf2684eb6a10f720545f593b1e9452be037549
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-15 10:38:45 -0800

    hello_world

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function.
+ *
+ * Return: If returns 0, function works well.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
