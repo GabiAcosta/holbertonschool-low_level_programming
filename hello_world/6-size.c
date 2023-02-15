4ad04bb3e82d86cd84f16746d7338d3bf4ff960f
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-15 11:06:49 -0800

    hello_world

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,13 @@ int main(void)
 {
 char cha;
 int in;
-long lon;
+long int lon;
+long long int lolo;
 float floa;
 printf("Size of a char: %zu byte(s)\n", sizeof(cha));
 printf("Size of an int: %zu byte(s)\n", sizeof(in));
-printf("Size of a long: %zu byte(s)\n", sizeof(lon));
+printf("Size of a long int: %zu byte(s)\n", sizeof(lon));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(lolo));
 printf("Size of a float: %zu byte(s)\n", sizeof(floa));
 return (0);
 }
