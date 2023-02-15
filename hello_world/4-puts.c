9df23b893a80adfa28a536b3d5f6b4f778a08c71
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-15 10:27:07 -0800

    hello_world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - This is the main function.
+ *
+ * Return: If returns 0, function works well.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
