8c6b09a0a82e95715e8a3f73f07981ae38202637
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-16 08:44:31 -0800

    variables_if_else_while

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char ch2 = 'A';
+	char nl = '\n';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (ch2 <= 'Z')
+	{
+		putchar(ch2);
+		ch2++;
+	}
+	putchar(nl);
+	return (0);
+}
