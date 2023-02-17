4ff914c8c3ef6863cd7d10e557c437bd9a9cb049
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-17 11:05:52 -0800

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints the sign of a number.
+ * @int: the number to check.
+ * Return: the absolute value of an integer.
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+
+		x = x * (-1);
+		return (x);
+	}
+	return (x);
+}
