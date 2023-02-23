46bd832185c9ba6a8400a1c037833af4f4b7d421
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-23 05:05:25 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
