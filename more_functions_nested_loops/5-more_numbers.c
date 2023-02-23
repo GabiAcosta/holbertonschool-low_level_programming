6077a8d54d5370c3a69d2ef73670d332713f898f
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-23 08:19:15 -0800

    fix program

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,16 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int n;
+	int i, n;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
 			if (n / 10 != 0)
+			{
 				_putchar(n / 10 + '0');
+			}
 				_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
