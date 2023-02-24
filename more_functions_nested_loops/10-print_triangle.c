dc00d97f2ded7f4d027012c23706e96768f91900
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-24 04:15:26 -0800

    fixed if size is 0 or less

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,18 +7,24 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size < 0)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (j < size - i - 1)
-			{
-				_putchar(' ');
-			} else
-			{
-				_putchar(35);
-			}
-		}
 		_putchar('\n');
+	} else
+	{	
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < size - i - 1)
+				{
+					_putchar(' ');
+				} else
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
