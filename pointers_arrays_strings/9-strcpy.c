d0a3b3dcd7e7c487312bfd3ffbbbca78415509b0
GabiAcosta <acostagabriel1311@gmail.com>
2023-02-28 14:45:35 -0800

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - function that copies a string
+ * @dest: string copy destination
+ * @src: the string to copy
+ * Return: returns copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (dest);
+}
