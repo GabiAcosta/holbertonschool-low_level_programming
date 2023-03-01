e1cbff52e185096dd7280f2ebdeca0ca592d43c3
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-01 11:14:01 -0800

    task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: int indicating the result of the comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int count = 0;
+
+	while (s1[count] != '\0' && s2[count] != '\0')
+	{
+		if (s1[count] != s2[count])
+		{
+			return (s1[count] - s2[count]);
+		}
+		count++;
+	}
+	return (s1[count] - s2[count]);
+}
