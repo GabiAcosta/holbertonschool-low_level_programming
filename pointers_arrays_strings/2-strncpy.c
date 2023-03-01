3979653ac523644c2b002b86c1187ecc0d08a3dc
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-01 10:29:07 -0800

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count = 0;
-	char *aux = dest;
 
-	if (n >= 0)
+	while (count < n && src[count] != '\0')
 	{
-		while ((dest[count]) && (n > 0))
-		{
-			dest[count] = src[count];
-			count++;
-			n--;
-		}
+		dest[count] = src[count];
+		count++;
 	}
-	return (aux);
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+	return (dest);
 }
