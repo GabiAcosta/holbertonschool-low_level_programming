0b7a407a6ceaab7efce3d81b7858649bdaa87b5c
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-02 08:31:03 -0800

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string to modify
+ * Return: string modified
+ */
+char *cap_string(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] == 32 || s[count] == '\t' || s[count] == '\n' || s[count] == ','
+		|| s[count] == ';' || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		|| s[count] == '"' || s[count] == '(' || s[count] == ')' || s[count] == '{'
+		|| s[count] == '}')
+		{
+			if (s[count + 1] >= 'a' && s[count + 1] <= 'z')
+			{
+				s[count + 1] = (s[count + 1] - 32);
+			}
+		}
+		count++;
+	}
+	return (s);
+}
