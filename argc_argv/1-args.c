6c4ef5ac6ecc40717c889a93edc50e45da774f0c
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-08 06:10:23 -0800

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
