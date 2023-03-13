29285cd168f767c3e30adb5d472b2ee16fcc9503
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-13 11:38:22 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *aux;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	len = _strlen(s1);
 	len2 = _strlen(s2);
 
