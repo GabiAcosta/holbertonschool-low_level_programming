abb0d147006d796bc280601f3222c858c303b400
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-20 07:59:25 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
