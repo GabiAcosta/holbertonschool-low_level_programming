df206671ec5fbb06c1f6f86f250a28838fe7c59c
GabiAcosta <acostagabriel1311@gmail.com>
2023-03-20 13:04:11 -0700

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs
+ * @d: name of the struct
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
