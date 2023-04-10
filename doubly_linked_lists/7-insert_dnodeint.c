d2e4ced7fb2038b8ae6363bfcc283569101b764e
GabiAcosta <acostagabriel1311@gmail.com>
2023-04-10 11:43:39 -0700

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,9 +12,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *temp2;
 	unsigned int i = 0;
 
-	if (!*h)
-		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
