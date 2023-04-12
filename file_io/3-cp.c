8234314f28eeb1e9d9d5840efa69afe564c1e950
GabiAcosta <acostagabriel1311@gmail.com>
2023-04-12 14:53:12 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,12 +19,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		close(fd_from);
-	}
 
 	while ((rd = read(fd_from, buffer, 1024)) > 0)
 	{
