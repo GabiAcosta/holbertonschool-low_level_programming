0099610ee85d22ac3d82aacc74e9086bcbbf70ca
GabiAcosta <acostagabriel1311@gmail.com>
2023-04-13 13:00:33 -0700

    task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key of the hash table
+ * Return: Returns the value associated with the element, or NULL if
+ * key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	char *str = NULL;
+
+	if (!ht || !key)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		str = ht->array[index]->value;
+	}
+	return (str);
+}
