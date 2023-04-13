a23ad113fefbc2a26bb733fdc6de99fb4e4db638
GabiAcosta <acostagabriel1311@gmail.com>
2023-04-13 11:47:51 -0700

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (strcmp(ht->array[index]->key, key) == 0)
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
 		return (1);
