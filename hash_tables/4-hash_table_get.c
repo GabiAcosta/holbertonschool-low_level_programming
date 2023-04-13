428c048eb9b87263c00c3ae4ee2db88063a30e0c
GabiAcosta <acostagabriel1311@gmail.com>
2023-04-13 13:16:31 -0700

    task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	char *str = NULL;
-	hash_node_t node;
+	hash_node_t *node;
 
 	if (!ht || !key)
 		return (0);
